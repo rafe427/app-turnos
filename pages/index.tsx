5fe81cceda3b4f995e61443d69e694b08fe59684
Rafael Ignacio Campagna <rafe427@gmail.com>
2025-05-05 23:07:14 -0300

    → Uso App Router: elimino carpeta pages

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-// pages/index.tsx
-export default function Home() {
-  return (
-    <main style={{ padding: 20, fontFamily: 'sans-serif' }}>
-      <h1>¡Bienvenido a App-Turnos!</h1>
-      <p>Esta es tu página de inicio.</p>
-    </main>
-  )
-}
